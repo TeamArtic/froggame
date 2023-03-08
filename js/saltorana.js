7da8cc20168f950cac8346a658fe57e07775b807
genexix05 <genexix05@gmail.com>
2023-03-08 13:06:14 +0100

    js

---
+++ js/saltorana.js
@@ -0,0 +1,24 @@
+var rana = document.getElementById("rana");
+		var rana1 = "../img/dona.gif";
+		var rana2 = "../img/saltar.gif";
+		var isRana1 = true;
+		var isJumping = false;
+
+		rana.addEventListener("click", function() {
+			if (!isJumping) {
+				isJumping = true;
+				rana.classList.add("jump");
+				if (isRana1) {
+					rana.src = rana2;
+					isRana1 = false;
+				} else {
+					rana.src = rana1;
+					isRana1 = true;
+				}
+				setTimeout(function() {
+					rana.classList.remove("jump");
+					rana.src = rana1;
+					isJumping = false;
+				}, 1000);
+			}
+		});
