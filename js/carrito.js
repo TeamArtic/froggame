bd1786efeb7c43e0d8357b73cbbf270c8552daa5
Blasep4 <blasep4@gmail.com>
2023-03-09 09:56:50 +0100

    cambio

--- js/carrito.js
+++ js/carrito.js
@@ -16,5 +16,32 @@ function abrircarrito() {
 function cerrarcarrito() {
   cartContainer.style.display = 'none';
 };
+// Obtener elementos del DOM
+const saldoSpan = document.getElementById('saldo');
+const botonesAñadir = document.querySelectorAll('.buy-button');
 
+// Saldo inicial
+let saldo = 100;
+
+// Actualizar el saldo en la página
+saldoSpan.textContent = saldo;
+
+// Función para actualizar el saldo en la página
+function actualizarSaldo() {
+  saldoSpan.textContent = saldo;
+}
+
+// Agregar manejadores de eventos para el botón "Añadir"
+botonesAñadir.forEach(function(boton) {
+  boton.addEventListener('click', function() {
+    // Obtener el precio del producto del botón
+    const precio = parseFloat(boton.dataset.price);
+
+    // Restar el precio del producto al saldo
+    saldo -= precio;
+
+    // Actualizar el saldo en la página
+    actualizarSaldo();
+  });
+});
 
