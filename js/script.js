f2b3ae2d10b65ef621452382ffdb9cd0008c118d
Blasep4 <blasep4@gmail.com>
2023-03-09 09:17:25 +0100

    cambios

--- js/script.js
+++ js/script.js
@@ -5,3 +5,14 @@ class level {
 window.addEventListener('load', () => {
 
 })
+const cartButton = document.querySelector('nav li:last-child');
+const cartContainer = document.querySelector('.cart-container');
+const cartCloseButton = document.querySelector('.cart-close-button');
+
+cartButton.addEventListener('click', function() {
+  cartContainer.style.display = 'block';
+});
+
+cartCloseButton.addEventListener('click', function() {
+  cartContainer.style.display = 'none';
+});
