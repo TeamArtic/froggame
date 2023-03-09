8644bbce9bcd6da3064707f29f2bdf7145ecf300
genexix05 <genexix05@gmail.com>
2023-03-09 09:42:13 +0100

    dwdwd

--- js/carrito.js
+++ js/carrito.js
@@ -9,15 +9,10 @@ const cartButton = document.querySelector(".cart-button");
 const closeButton = document.querySelector(".close-button");
 const cartContainer = document.querySelector(".cart-container");
 
-cartButton.addEventListener("click", function() {
-  cartContainer.classList.add("open");
-});
-closeButton.addEventListener("click", function() {
-  cartContainer.classList.remove("open");
-});
-cartCloseButton.addEventListener('click', function() {
-  cartContainer.style.display = 'none';
-});
+function abrircarrito() {
+  cartContainer.style.display = 'block';
+};
+
 function cerrarcarrito() {
   cartContainer.style.display = 'none';
 };
