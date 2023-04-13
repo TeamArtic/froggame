21e34ec508113e7a0851e1ebd2660bbfc95ec794
KevinRCDE <127109022+KevinRCDE@users.noreply.github.com>
2023-04-13 14:19:15 +0200

    Añade el archivo "toggleMenu.js"

---
+++ js/toggleMenu.js
@@ -0,0 +1,14 @@
+class toggleMenu{
+    constructor(menu, activatedClass){
+        this.menu = menu
+        this.activatedClass = activatedClass
+    }
+
+    toggle(){
+        if(this.menu.classList.contains(this.activatedClass)){
+            this.menu.classList.remove(this.activatedClass)
+        }else{
+            this.menu.classList.add(this.activatedClass)
+        }
+    }
+}
