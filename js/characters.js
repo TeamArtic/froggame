c484b6abbddae8818a9a704686eb5adf55f69084
Blasep4 <blasep4@gmail.com>
2023-05-04 11:24:14 +0200

    arreglo personajes

--- js/characters.js
+++ js/characters.js
@@ -1,3 +1,10 @@
 function setCharacter(newCharacter){
     localStorage.setItem("selectedCharacterId", newCharacter)
 }
+function setCharacter(characterIndex) {
+    // ... código que selecciona un personaje ...
+  
+    // muestra el botón de cancelar
+    const cancelButton = document.getElementById('cancelar');
+    cancelButton.classList.remove('hidden');
+  }
