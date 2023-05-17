e3acdf2d7ba7c5598b5f9b03c1f6becb8bb92b60
Blas Escalona Perez <blasep4@gmail.com>
2023-05-17 19:29:32 +0200

    botones compra personajes

--- js/characters.js
+++ js/characters.js
@@ -8,3 +8,16 @@ function setCharacter(characterIndex) {
     const cancelButton = document.getElementById('cancelar');
     cancelButton.classList.remove('hidden');
   }
+  function setCharacter(characterIndex) {
+    var buttons = document.getElementsByClassName("highlighted-button");
+    var selectedButton = buttons[characterIndex];
+  
+    // Cambiar el texto y estado del botón seleccionado
+    selectedButton.innerHTML = "Comprado";
+    selectedButton.disabled = true;
+    selectedButton.style.backgroundColor = "gray";
+    selectedButton.style.transform = "none";
+    selectedButton.style.color = "black";
+    selectedButton.style.textShadow = "none";
+    selectedButton.style.boxShadow = "none";
+  }
