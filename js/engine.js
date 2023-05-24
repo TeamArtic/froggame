608178834e9efc15f1329c4cd479e55a3acbec89
genexix05 <genexix05@gmail.com>
2023-05-24 10:30:04 +0200

    dasdasda

--- js/engine.js
+++ js/engine.js
@@ -227,11 +227,8 @@ class gridObject extends object{
     }
 }
 
-var lastTouchEnd = 0;
-document.addEventListener('touchend', function (event) {
-  var now = (new Date()).getTime();
-  if (now - lastTouchEnd <= 300) {
-    event.preventDefault();
-  }
-  lastTouchEnd = now;
-}, false);
+document.addEventListener('touchmove', function(event) {
+    if (event.scale !== 1) {
+      event.preventDefault();
+    }
+  }, { passive: false });
