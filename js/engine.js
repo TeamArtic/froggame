7abfaa225c4aba004888a0c3880f17d545c1f402
genexix05 <genexix05@gmail.com>
2023-05-24 09:31:10 +0200

    quitar doble tap iphone

--- js/engine.js
+++ js/engine.js
@@ -226,3 +226,12 @@ class gridObject extends object{
         }
     }
 }
+
+var lastTouchEnd = 0;
+document.addEventListener('touchend', function (event) {
+  var now = (new Date()).getTime();
+  if (now - lastTouchEnd <= 300) {
+    event.preventDefault();
+  }
+  lastTouchEnd = now;
+}, false);
