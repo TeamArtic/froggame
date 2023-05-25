92a671c8b3ae79b5df9505956945ec97c5c423b8
KevinRCDE <127109022+KevinRCDE@users.noreply.github.com>
2023-05-25 22:32:54 +0200

    Restore archievements scripts

---
+++ js/archievementsRendering.js
@@ -0,0 +1,13 @@
+let container
+let localArchievementsArray = getArchievement()
+
+window.addEventListener('load', () => {
+    container = document.getElementById('container')
+    for(let i = 0; i < localArchievementsArray.length; i++){
+        if(localArchievementsArray[i].value){
+            container.innerHTML += "<div class=\"unlockedArchievement vertical-menu\"><h2>" + archievementsArrayInfo[i].UIName + "</h2><p>" + archievementsArrayInfo[i].description + "</p></div>"
+        }else{
+            container.innerHTML += "<div class=\"lockedArchievement vertical-menu\"><h2>(Bloqueado)" + archievementsArrayInfo[i].UIName + "</h2><p>" + archievementsArrayInfo[i].description + "</p></div>"
+        }
+    }
+})
