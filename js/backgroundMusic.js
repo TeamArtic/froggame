de1a3283a48027d4ec63f72d0d4ac61126236dd7
KevinRCDE <127109022+KevinRCDE@users.noreply.github.com>
2023-05-25 08:39:19 +0200

    Add backgroundMusic.js

---
+++ js/backgroundMusic.js
@@ -0,0 +1,72 @@
+class backgroundAudioController{
+    constructor(audioURL, button, volume){
+        this.button = button
+        this.audio = new Audio(audioURL)
+        // this.audio.volume = parseFloat(volume / 100)
+        // this.audio.volume = volume
+        this.volume = volume
+        button.addEventListener('click', () => {
+            if(!automaticStartedAudio){
+                this.pause()
+                automaticStartedAudio = true
+            }else{
+                if(this.audio.paused){
+                    this.play()
+                    // this.audio.play()
+                    // this.button.innerHTML = "<span class=\"material-symbols-outlined\">" +
+                    // "volume_up" +
+                    // "</span>" // TODO Do this part of the code in a clearner and modular way.
+                    // this.button.value = "Parar" // TODO Do this part of the code in a clearner and modular way.
+
+                }else{
+                    this.pause()
+                    // this.audio.stop()
+                    // this.audio.pause()
+                    // this.button.innerHTML = "<span class=\"material-symbols-outlined\">" +
+                    // "volume_off" +
+                    // "</span>"
+                    // this.button.value = "Reproducir"
+                }
+            }
+        })
+        volume.addEventListener('input', () => {
+            // this.audio.volume = volume.value / 100
+            if(volume.value == 0){
+                this.pause()
+            }else if(this.audio.paused && volume.value != 0){
+                this.play()
+                this.audio.volume = volume.value / 100
+            }else{
+                this.audio.volume = volume.value / 100
+            }
+        })
+        volume.addEventListener('click', () => {
+            if(volume.value != 0 && this.audio.paused){
+                this.play()
+            }
+        })
+        this.audio.addEventListener('ended', () => {
+            this.audio.currentTime = 0;
+            this.play()
+        })
+    }
+
+    play(){
+        this.audio.play()
+        this.button.innerHTML = "<span class=\"material-symbols-outlined\">" +
+        "volume_up" +
+        "</span>" // TODO Do this part of the code in a clearner and modular way.
+
+    }
+
+    pause(){
+        this.audio.pause()
+        this.button.innerHTML = "<span class=\"material-symbols-outlined\">" +
+        "volume_off" +
+        "</span>"
+    }
+
+    autoPlay(){
+        this.audio.play()
+    }
+}
